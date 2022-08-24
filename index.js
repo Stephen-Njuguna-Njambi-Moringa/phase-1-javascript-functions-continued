cc5cb17c5f8605b761b5bbbbb819354b0ae05368
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-08-24 21:59:57 +0300

    lab passed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// code your solution here
+// code your solution here'
+function saturdayFun(action){
+    if(typeof action==='undefined'){
+        action="roller-skate";
+    }
+    return `This Saturday, I want to ${action}!`
+}
+saturdayFun();
+function mondayWork(action1='go to the office'){
+    return `This Monday, I will ${action1}.`;
+    
+}
+mondayWork();
+
+function wrapAdjective(pass){
+    return function(){
+        if(pass==='||'){
+            let results='||a dedicated programmer||!';
+            let emphatic=`You are ${results}`;
+            return emphatic;
+        }  
+
+        else if(pass==='*'){
+            let results='*a hard worker*!';
+            let emphatic=`You are ${results}`;
+            return emphatic;
+        }
+    }
+
+}
